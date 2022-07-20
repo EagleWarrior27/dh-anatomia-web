6edf803bd22997016c9047a64767b5662ae8ae04
CarlosUcan <carlos.ucan@chapur.com>
2022-07-20 18:18:06 -0500

    Estructura del sitio web

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ app.use(express.static(public));
 app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
 });
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
